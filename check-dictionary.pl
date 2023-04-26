eaee64583199a1cccebcaa425e8f369dbcbf1062
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-25 20:51:46 -0400

    Use sentences for errors

--- check-dictionary.pl
+++ check-dictionary.pl
@@ -16,12 +16,12 @@ while ($content =~ s/([^\r\n\x0b\f\x85\x2028\x2029]*)(\r\n|\n|\r|\x0b|\f|\x85|\x
     unless (defined $first_end) {
         $first_end = $end;
     } elsif ($end ne $first_end) {
-        print WARNINGS "$file:$.:$-[0] ... $+[0], Warning - entry has inconsistent line ending (unexpected-line-ending)\n";
+        print WARNINGS "$file:$.:$-[0] ... $+[0], Warning - Entry has inconsistent line ending. (unexpected-line-ending)\n";
     }
     if ($line =~ '"/^[${expected_chars}]*([^${expected_chars}]+)/"') {
         $column_range="$-[1] ... $+[1]";
         unless ($line =~ '"/^${comment_char}/"') {
-        print WARNINGS "$file:$.:$column_range, Warning - ignoring entry because it contains non alpha characters (non-alpha-in-dictionary)\n";
+        print WARNINGS "$file:$.:$column_range, Warning - Ignoring entry because it contains non alpha characters. (non-alpha-in-dictionary)\n";
         }
         $line = "";
     }
